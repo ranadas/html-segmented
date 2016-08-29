a74facaa8327beb5bcd19a652f380ebe6c5ee8bd
Rana Das <rana.pratap.das@gmail.com>
2016-08-29 06:32:55 +0100

    dynamic, search , sorting, hide when not available  etc.

--- app-ng.js
+++ app-ng.js
@@ -6,23 +6,28 @@
 // the second parameter is needed to declare for the first time.
 var app = angular.module("AngGettingStarted", []);
 
+var displayClass = {
+    message: "Hello World, GitHub Viewer",
+    username: "angular",
+    repos:[]
+};
+
 //2. add a controller : add to the module variable
 app.controller("mainController", function ($scope) {
-    var displayClass = {
-        message: "Hello Rana Das"
-    };
-
-
-    console.log(displayClass.message);
+    console.log("in main controller with" + JSON.stringify(displayClass));
     $scope.data = displayClass;
 });
 
 
 // 3. create a function
 var UserController = function ($scope, $http) {
-    console.log("in user controller");
+    console.log("in user controller with " + JSON.stringify(displayClass));
+    $scope.data = displayClass;
     var onUserComplete = function (response) {
+        console.log("Search result " + JSON.stringify(response.data));
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
     };
 
 
@@ -31,9 +36,19 @@ var UserController = function ($scope, $http) {
         $scope.error = reason.statusText;
     };
 
+    var searchFunction = function (uname) {
+        console.log("searching for " + uname);
+        $http.get("https://api.github.com/users/" + uname)
+            .then(onUserComplete, onError);
+    };
 
-    $http.get("https://api.github.com/users/ranadas")
-        .then(onUserComplete, onError);
+    var onRepos = function (response) {
+        console.log("repos result " + JSON.stringify(response.data));
+        $scope.data.repos = response.data;
+    };
+
+    $scope.search = searchFunction;
+    $scope.repoSortOrder = "-language";
 };
 //add a controller by add to the module variable.
 app.controller("userController", ["$scope", "$http", UserController]);
